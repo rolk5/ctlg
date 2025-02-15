96b6e24d29258f9edf968777b51f0baaf5769514
rolk5 <95336456+rolk5@users.noreply.github.com>
2025-02-15 20:44:29 +0700

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,18 +1,18 @@
 
 const DATA_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=tuQdiLD1YG42imxTO7r9hYD0y3L6GsSiKcOwLZs75Nd5nGl-gPFPzGn7HcUv4SK7TMQOzHXM8BauLQIepIc4fR-pQmHWmXjpm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPAA2y3VG9JL81ZNanfkRoRM1TRpNg5VgNchTapj0eLsNmUatjGXwvNYTINa6Bd7jMjkfAoyZeN7HHAcHOJxqh3IbVrF9IR6-w&lib=MvIYJgZ0MgXa3kditUeoakMBHEyKE_rmM';
 
-    // Функция группировки данных
-    function groupData(data) {
-            return data.reduce((acc, item) => {
-                if (!acc[item.Тип]) acc[item.Тип] = { };
-                if (!acc[item.Тип][item.Группа]) acc[item.Тип][item.Группа] = [];
-                acc[item.Тип][item.Группа].push(item.Название);
-    return acc;
+// Функция группировки данных
+function groupData(data) {
+    return data.reduce((acc, item) => {
+        if (!acc[item.Тип]) acc[item.Тип] = {};
+        if (!acc[item.Тип][item.Группа]) acc[item.Тип][item.Группа] = [];
+        acc[item.Тип][item.Группа].push(item.Название);
+        return acc;
 
-            }, { });
-        }
+    }, {});
+}
 
-    // Функция отрисовки (без изменений)
+// Функция отрисовки (без изменений)
 function renderData(groupedData) {
     const container = document.getElementById('data-container');
 
@@ -53,19 +53,20 @@ function renderData(groupedData) {
     }
 }
 
-    // Загрузка данных при старте
-    window.onload = function() {
-        fetch(DATA_URL)
-            .then(response => {
-                if (!response.ok) throw new Error('Ошибка сети');
-                return response.json();
-            })
-            .then(data => {
-                const groupedData = groupData(data.users);
-                renderData(groupedData);
-            })
-            .catch(error => {
-                console.error('Ошибка загрузки данных:', error);
-                newElement.innerHTML = '<p>Ошибка загрузки данных</p>';
-                });
-        };
+// Загрузка данных при старте
+window.onload = function () {
+    fetch(DATA_URL)
+        .then(response => {
+            if (!response.ok) throw new Error('Ошибка сети');
+            return response.json();
+        })
+        .then(data => {
+            const groupedData = groupData(data.users);
+            renderData(groupedData);
+        })
+        .catch(error => {
+            console.error('Ошибка загрузки данных:', error);
+            newElement.innerHTML = '<p>Ошибка загрузки данных</p>';
+        });
+};
+
